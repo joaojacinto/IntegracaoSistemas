cdd6d486580c9f5ddb3ae6cf63882d66015763fc
joaojacinto <joaorsj96@gmail.com>
2023-01-18 17:11:47 +0000

    database

---
+++ valorant_db/src/main/java/com/example/valorant_db/ValorantDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.valorant_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantDbApplication.class, args);
+	}
+
+}
---
+++ valorant_db/src/test/java/com/example/valorant_db/ValorantDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.valorant_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
